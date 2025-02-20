92398ca457df77521fd81810e5bffc871c67e419
Pavel Kurial <kuriapav@fit.cvut.cz>
2025-02-20 11:03:00 +0100

    Hotifx Many2Many

--- backend/models/Match.go
+++ backend/models/Match.go
@@ -1,7 +1,8 @@
 package models
 
 type Match struct {
-	MatchID uint    `gorm:"primaryKey" json:"match_id"`
-	Comp    Comp    `gorm:"foreignKey:comp_id"`
-	Teams   []*Team `gorm:"many2many:match_team" json:"teams"`
+	MatchID uint     `gorm:"primaryKey" json:"match_id"`
+	Comp    Comp     `gorm:"foreignKey:comp_id"`
+	Teams   []*Team  `gorm:"many2many:match_team" json:"teams"`
+	Tracks  []*Track `gorm:"many2many:track_match"`
 }
